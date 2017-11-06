af4a9b60a11d9a595b0b37b73e86da14ad25bdea
WillGreen98 <will.green98@hotmail.com>
2017-11-06 23:21:10 +0000

    Week 5 Update

--- Week 5/Practical_5.py
+++ Week 5/Practical_5.py
@@ -37,8 +37,13 @@ def drawBrownEye(window, centre, radius):
     drawBrownEye(window, g.Point(centre, 100), radius / 2, "brown")
     drawBrownEye(window, g.Point(centre, 100), radius / 4, "black")
 
+def drawBrownEye_NoPoint(window, centre, radius):
+    drawBrownEye(window, centre, radius, "white")
+    drawBrownEye(window, centre, radius / 2, "brown")
+    drawBrownEye(window, centre, radius / 4, "black")
 
-def drawPairBrownEyes(window, centre, radius):
+
+def drawBrownEyes_Pair(window, centre, radius):
     window = g.GraphWin()
 
     drawBrownEye(window, g.Point(90, 100))
@@ -62,3 +67,28 @@ def drawLetter_A():
     drawStars_Gap(0, 10, 0, 0, 2)
     drawStars_Gap(0, 2, 6, 2, 3)
 
+def drawBrownEyes_Two_Pairs():
+    window = g.GraphWin("4 Eyes", 1000, 1000)
+
+    for i in range(4):
+        coord_Left = window.getMouse()
+        coord_Right = coord_Left.getX() + radius
+
+        radius = distBetweenPoints(coord_Left, window.getMouse())
+        drawBrownEye_NoPoint(window, coord_Left, radius)
+        drawBrownEye_NoPoint(window, g.Point(coord_Left.getY(), coord_Right),
+                             radius)
+        window.getMouse()
+
+def displayTextWithSpaces(window, text, size, pos):
+    text = text.replace("", " ")
+    text_Display = g.Text(pos, text).draw(window).setSize(size)
+
+def constructVisionChart():
+    window = g.GraphWin("Chart", 400, 400)
+
+    font_Sizes = [30, 25, 20, 15, 10, 5]
+    dx = 100
+    dy = 50
+
+    for size in font_Sizes:
