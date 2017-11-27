dca6c95b95cc0a8f1d4e3e95a1afabea1d18af52
WillGreen98 <will.green98@hotmail.com>
2017-11-27 12:49:38 +0000

    Week 8

--- Week 8/coin_flip.py
+++ Week 8/coin_flip.py
@@ -1,7 +1,29 @@
 import random as rand
 
+def getInputs():
+    while True:
+        n_flips = int(input("How many coin flips: "))
+
+        if n_flips.isnumeric():
+            return n_flips
+
+def flip(index):
+    flips = []
+    for i in range(index):
+        flips.append(rand.randint(0, 1))
+    return flips
+
+
+def results(result):
+    head_counter = result.count(0) / 100
+    tail_counter = result.count(1) / 100
+
+    print("Heads: ", head_counter, " Tails: ", tail_counter)
+
 def main():
-    a = 5
+    index = getInputs()
+    flips = flip(index)
+    results(flips)
 
 if __name__ == '__main__':
     main()
