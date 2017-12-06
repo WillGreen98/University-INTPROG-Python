59bcade7b03a76355451e7bb6bb5638b1cb4a974
WillGreen98 <will.green98@hotmail.com>
2017-12-06 16:34:50 +0000

    Pattern F & Layout Update

--- Course Work/pattern.py
+++ Course Work/pattern.py
@@ -2,11 +2,8 @@ __author__ = "Will - UP853829"
 __project__ = "Patterns Course Word"
 
 import graphics as g
-import numbers
 
-DEBUG = False
-
-valid_colours = ["red", "orange", "green", "blue", "magenta", "cyan", "brown", "pink"]  # Order of Freq
+valid_colours = ["red", "orange", "green", "blue", "magenta", "cyan", "brown", "pink"]  # PINK ISN'T A COLOUR!!!
 
 def getInput():
     val_Dimensions = [5, 7, 9, 11]
@@ -15,7 +12,7 @@ def getInput():
     while True:
         dimension = input("Enter dimension: ")
 
-        if dimension.isnumeric(): # __contains__(str(filter(lambda i: isinstance(i, numbers.Number), val_Dimensions))):
+        if dimension.isnumeric():
             dimension = int(dimension)
             if dimension % 2 == 1:
                 if dimension in val_Dimensions:
@@ -43,32 +40,41 @@ def getInput():
     return dimension, colour_Choices
 
 def draw_Patch_Penultimate(window, pos_X, pos_Y, colour, reversed):
-    point_One = g.Point(0, 0)
-    point_Two = g.Point(50, 0)
-    point_Three = g.Point(50, 50)
+    # point_One = g.Point(0, 0)
+    # point_Two = g.Point(50, 0)
+    # point_Three = g.Point(50, 50)
+    #
+    # if reversed == False:
+    #     for i in range(5):
+    #         if i % 2 == 0:
+    #             colour = colour
+    #         else:
+    #             colour = "White"
+    #
+    #         point_One = g.Point(pos_X + 10, pos_Y)
+    #         point_Two = g.Point(pos_X, pos_Y)
+    #         point_three = g.Point(pos_X, pos_Y - 10)
+    # elif reversed == True:
+    #     for i in range(5):
+    #         if i % 2 == 1:
+    #             colour = "white"
+    #         else:
+    #             colour = colour
+    #
+    #         point_One = g.Point(pos_X - 10, pos_Y)
+    #         point_Two = g.Point(pos_X, pos_Y)
+    #         point_three = g.Point(pos_X, pos_Y + 10)
+    #
+    # poly = g.Polygon(point_One, point_Two, point_Three).draw(window).setFill(colour)
 
-    if reversed == False:
-        for i in range(5):
-            if i % 2 == 0:
-                colour = colour
-            else:
-                colour = "White"
+    for i in range(0, 50, 10):
+        p1 = g.Point(pos_X + i, pos_Y + 150)
+        p2 = g.Point(pos_X + 150 + i, pos_Y)
+        p3 = g.Point(pos_X + 50, pos_Y + i)
+        p4 = g.Point(pos_X, pos_Y + 50 + i)
 
-            point_One = g.Point(pos_X + 10, pos_Y)
-            point_Two = g.Point(pos_X, pos_Y)
-            point_three = g.Point(pos_X, pos_Y - 10)
-    elif reversed == True:
-        for i in range(5):
-            if i % 2 == 1:
-                colour = "white"
-            else:
-                colour = colour
+        polygon = g.Polygon(p1, p2, p3, p4).draw(window).setFill(colour)
 
-            point_One = g.Point(pos_X - 10, pos_Y)
-            point_Two = g.Point(pos_X, pos_Y)
-            point_three = g.Point(pos_X, pos_Y + 10)
-
-    poly = g.Polygon(point_One, point_Two, point_Three).draw(window).setFill(colour)
 
 def drawLine(window, pos_X, pos_Y, colour):
     line = g.Line(pos_X, pos_Y).draw(window).setFill(colour)
@@ -82,28 +88,23 @@ def draw_Patch_Final(window, pos_X, pos_Y, colour):
 
 def main():
     x = 0
-    cols_Counter = 0
-    rows_Counter = 0
 
-    if DEBUG:
-        window = g.GraphWin("Patch Work")
-        draw_Patch_Penultimate(window, 200, 200, "red", False)
+    dim, col_Array = getInput()
+    window = g.GraphWin("Patch Work", dim * 100, dim * 100)
 
-        # draw_Patch_Final(window, 50, 50, "red")
-        # draw_Patch_Final(window, 180, 180, "black")
-        # draw_Patch_Final(window, 300, 250, "blue")
-    else:
-        dim, col = getInput()
-        window = g.GraphWin("Patch Work", dim * 100, dim * 100)
+    patch_X = window.getHeight() + 300
+    patch_Y = window.getHeight() - 300
 
-        for cols in range(0, window.getHeight(), 100):
-            for rows in range(0, window.getWidth(), 100):
-                #draw_Patch_Penultimate(window, rows, cols, col[x], True)
-                draw_Patch_Final(window, rows, cols, col[x])
-
-                x += 1
-                if x == 3:
-                    x = 0
+    for columns in range(0, window.getHeight(), 100):
+        for rows in range(0, window.getWidth(), 100):
+            if rows <= 299 and columns >= window.getWidth() - 300:
+                draw_Patch_Final(window, rows, columns, col_Array[x])
+            else:
+                draw_Patch_Penultimate(window, rows, columns, col_Array[x], False)
+                pass
+            x += 1
+            if x == 3:
+                x = 0
 
     window.getMouse()
 
