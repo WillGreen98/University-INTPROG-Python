637302223d771fdfaf1ab729efb112a8f6edd3d8
WillGreen98 <will.green98@hotmail.com>
2017-12-06 22:47:00 +0000

    Pattern Pen Update

--- Course Work/pattern.py
+++ Course Work/pattern.py
@@ -39,41 +39,76 @@ def getInput():
 
     return dimension, colour_Choices
 
-def draw_Patch_Penultimate(window, pos_X, pos_Y, colour, reversed):
-    # point_One = g.Point(0, 0)
-    # point_Two = g.Point(50, 0)
-    # point_Three = g.Point(50, 50)
-    #
-    # if reversed == False:
-    #     for i in range(5):
-    #         if i % 2 == 0:
-    #             colour = colour
-    #         else:
-    #             colour = "White"
-    #
-    #         point_One = g.Point(pos_X + 10, pos_Y)
-    #         point_Two = g.Point(pos_X, pos_Y)
-    #         point_three = g.Point(pos_X, pos_Y - 10)
-    # elif reversed == True:
-    #     for i in range(5):
-    #         if i % 2 == 1:
-    #             colour = "white"
-    #         else:
-    #             colour = colour
-    #
-    #         point_One = g.Point(pos_X - 10, pos_Y)
-    #         point_Two = g.Point(pos_X, pos_Y)
-    #         point_three = g.Point(pos_X, pos_Y + 10)
-    #
-    # poly = g.Polygon(point_One, point_Two, point_Three).draw(window).setFill(colour)
+def drawPolygon(window, colour, p1, p2, p3):
+    poly = g.Polygon(p1, p2, p3)
+    poly.setOutline(colour)
+    poly.setFill(colour)
+    poly.draw(window)
 
-    for i in range(0, 50, 10):
-        point_One = g.Point(pos_X + i, pos_Y + 10)
-        point_Two = g.Point(pos_X + 10 + i, pos_Y)
-        point_Three = g.Point(pos_X + 50, pos_Y + i)
-        point_Four = g.Point(pos_X, pos_Y + 50 + i)
+def colour_Picker(colour_In, colour):
+    if colour == colour_In:
+        colour = "white"
+    else:
+        colour = colour_In
 
-        polygon = g.Polygon(point_One, point_Two, point_Three, point_Four).draw(window).setFill(colour)
+    return colour
+
+def draw_Patch_Penultimate(window, pos, colour_In):
+    p1 = g.Point(pos.getX() + 50, pos.getY() + 50)
+    p2 = g.Point(pos.getX() + 50, pos.getY() + 100)
+    p3 = g.Point(pos.getX(), pos.getY() + 100)
+    p4 = g.Point(pos.getX(), pos.getY() + 50)
+    p5 = g.Point(pos.getX(), pos.getY())
+    p6 = g.Point(pos.getX() + 50, pos.getY())
+    p7 = g.Point(pos.getX() + 100, pos.getY())
+    p8 = g.Point(pos.getX() + 100, pos.getY() + 50)
+    p9 = g.Point(pos.getX() + 100, pos.getY() + 100)
+
+    colour = colour_In
+    for i in range(8):
+        pos_Increment = 0
+        if i == 0:
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, g.Point(p5.getX(), p5.getY() + pos_Increment), g.Point(p1.getX() - pos_Increment, p1.getY()), p4)
+                pos_Increment += 10
+        elif i == 1:
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, g.Point(p5.getX() + pos_Increment, p5.getY()), g.Point(p1.getX(), p1.getY() - pos_Increment), p6)
+                pos_Increment += 10
+        elif i == 2:
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, p6, g.Point(p7.getX() - pos_Increment, p7.getY()), g.Point(p1.getX(), p1.getY() - pos_Increment))
+                pos_Increment += 10
+        elif i == 3:
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, g.Point(p7.getX(), p7.getY() + pos_Increment), g.Point(p1.getX() + pos_Increment, p1.getY()), p8)
+                pos_Increment += 10
+        elif i == 4:
+            colour = "white"
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, g.Point(p3.getX(), p3.getY() - pos_Increment), g.Point(p1.getX() - pos_Increment, p1.getY()), p4)
+                pos_Increment += 10
+        elif i == 5:
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, g.Point(p3.getX() + pos_Increment, p3.getY()), g.Point(p1.getX(), p1.getY() + pos_Increment), p2)
+                pos_Increment += 10
+        elif i == 6:
+            colour = colour_In
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, p8, g.Point(p1.getX() + pos_Increment, p1.getY()), g.Point(p9.getX(), p9.getY() - pos_Increment))
+                pos_Increment += 10
+        elif i == 7:
+            for j in range(5):
+                colour = colour_Picker(colour_In, colour)
+                drawPolygon(window, colour, g.Point(p9.getX() - pos_Increment, p9.getY()), g.Point(p1.getX(), p1.getY() + pos_Increment), p2)
+                pos_Increment += 10
 
 
 def drawLine(window, pos_X, pos_Y, colour):
@@ -92,16 +127,12 @@ def main():
     dim, col_Array = getInput()
     window = g.GraphWin("Patch Work", dim * 100, dim * 100)
 
-    patch_X = window.getHeight() + 300
-    patch_Y = window.getHeight() - 300
-
     for columns in range(0, window.getHeight(), 100):
         for rows in range(0, window.getWidth(), 100):
             if rows <= 299 and columns >= window.getWidth() - 300:
                 draw_Patch_Final(window, rows, columns, col_Array[x])
             else:
-                draw_Patch_Penultimate(window, rows, columns, col_Array[x], False)
-                pass
+                draw_Patch_Penultimate(window, g.Point(rows, columns), col_Array[x])
             x += 1
             if x == 3:
                 x = 0
