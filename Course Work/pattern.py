4fdf0fd7f0872bd5c70e75557d4ebf7bc21667ba
WillGreen98 <will.green98@hotmail.com>
2017-12-08 13:19:11 +0000

    Pattern Organised Points Update

--- Course Work/pattern.py
+++ Course Work/pattern.py
@@ -55,14 +55,14 @@ def colour_Picker(current_Colour, colour):
     return colour
 
 def draw_Patch_Penultimate(window, pos_X, pos_Y, current_Colour):
-    mid = g.Point(pos_X + 50, pos_Y + 50)
-    mid_Bottom = g.Point(pos_X + 50, pos_Y + 100)
-    bottom_Left = g.Point(pos_X, pos_Y + 100)
-    mid_Left = g.Point(pos_X, pos_Y + 50)
     top_Left = g.Point(pos_X, pos_Y)
-    mid_Top = g.Point(pos_X + 50, pos_Y)
+    top_Mid = g.Point(pos_X + 50, pos_Y)
     top_Right = g.Point(pos_X + 100, pos_Y)
+    mid_Left = g.Point(pos_X, pos_Y + 50)
+    mid_Mid = g.Point(pos_X + 50, pos_Y + 50)
     mid_Right = g.Point(pos_X + 100, pos_Y + 50)
+    bottom_Left = g.Point(pos_X, pos_Y + 100)
+    bottom_Mid = g.Point(pos_X + 50, pos_Y + 100)
     bottom_Right = g.Point(pos_X + 100, pos_Y + 100)
 
     colour = current_Colour
@@ -71,44 +71,44 @@ def draw_Patch_Penultimate(window, pos_X, pos_Y, current_Colour):
         if quadrant == 0:
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, g.Point(top_Left.getX(), top_Left.getY() + pos_Increment), g.Point(mid.getX() - pos_Increment, mid.getY()), mid_Left, colour)
+                draw_Polygon(window, g.Point(top_Left.getX(), top_Left.getY() + pos_Increment), g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour)
                 pos_Increment += 10
         elif quadrant == 1:
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, g.Point(top_Left.getX() + pos_Increment, top_Left.getY()), g.Point(mid.getX(), mid.getY() - pos_Increment), mid_Top, colour)
+                draw_Polygon(window, g.Point(top_Left.getX() + pos_Increment, top_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), top_Mid, colour)
                 pos_Increment += 10
         elif quadrant == 2:
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, mid_Top, g.Point(top_Right.getX() - pos_Increment, top_Right.getY()), g.Point(mid.getX(), mid.getY() - pos_Increment), colour)
+                draw_Polygon(window, top_Mid, g.Point(top_Right.getX() - pos_Increment, top_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), colour)
                 pos_Increment += 10
         elif quadrant == 3:
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, g.Point(top_Right.getX(), top_Right.getY() + pos_Increment), g.Point(mid.getX() + pos_Increment, mid.getY()), mid_Right, colour)
+                draw_Polygon(window, g.Point(top_Right.getX(), top_Right.getY() + pos_Increment), g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), mid_Right, colour)
                 pos_Increment += 10
         elif quadrant == 4:
             colour = "white"
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, g.Point(bottom_Left.getX(), bottom_Left.getY() - pos_Increment), g.Point(mid.getX() - pos_Increment, mid.getY()), mid_Left, colour)
+                draw_Polygon(window, g.Point(bottom_Left.getX(), bottom_Left.getY() - pos_Increment), g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour)
                 pos_Increment += 10
         elif quadrant == 5:
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, g.Point(bottom_Left.getX() + pos_Increment, bottom_Left.getY()), g.Point(mid.getX(), mid.getY() + pos_Increment), mid_Bottom, colour)
+                draw_Polygon(window, g.Point(bottom_Left.getX() + pos_Increment, bottom_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour)
                 pos_Increment += 10
         elif quadrant == 6:
             colour = current_Colour
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, mid_Right, g.Point(mid.getX() + pos_Increment, mid.getY()), g.Point(bottom_Right.getX(), bottom_Right.getY() - pos_Increment), colour)
+                draw_Polygon(window, mid_Right, g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), g.Point(bottom_Right.getX(), bottom_Right.getY() - pos_Increment), colour)
                 pos_Increment += 10
         elif quadrant == 7:
             for i in range(5):
                 colour = colour_Picker(current_Colour, colour)
-                draw_Polygon(window, g.Point(bottom_Right.getX() - pos_Increment, bottom_Right.getY()), g.Point(mid.getX(), mid.getY() + pos_Increment), mid_Bottom, colour)
+                draw_Polygon(window, g.Point(bottom_Right.getX() - pos_Increment, bottom_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour)
                 pos_Increment += 10
 
 def drawLine(window, pos_X, pos_Y, colour):
--- Course Work/pen patch.py
+++ Course Work/pen patch.py
@@ -20,29 +20,51 @@ def colour_Picker(current_Colour, colour):
     return colour
 
 def draw_Patch_Penultimate(window, pos_X, pos_Y, current_Colour):
-    mid = g.Point(pos_X + 50, pos_Y + 50)
-    mid_Bottom = g.Point(pos_X + 50, pos_Y + 100)
-    bottom_Left = g.Point(pos_X, pos_Y + 100)
-    mid_Left = g.Point(pos_X, pos_Y + 50)
     top_Left = g.Point(pos_X, pos_Y)
-    mid_Top = g.Point(pos_X + 50, pos_Y)
+    top_Mid = g.Point(pos_X + 50, pos_Y)
     top_Right = g.Point(pos_X + 100, pos_Y)
+    mid_Left = g.Point(pos_X, pos_Y + 50)
+    mid_Mid = g.Point(pos_X + 50, pos_Y + 50)
     mid_Right = g.Point(pos_X + 100, pos_Y + 50)
+    bottom_Left = g.Point(pos_X, pos_Y + 100)
+    bottom_Mid = g.Point(pos_X + 50, pos_Y + 100)
     bottom_Right = g.Point(pos_X + 100, pos_Y + 100)
 
     colour = current_Colour
     pos_Increment = 0
 
     poly_List = {
-        unwrap.__call__(draw_Polygon, [window, g.Point(top_Left.getX(), top_Left.getY() + pos_Increment), g.Point(mid.getX() - pos_Increment, mid.getY()), mid_Left, colour]),
-        unwrap.__call__(draw_Polygon, [window, g.Point(top_Left.getX() + pos_Increment, top_Left.getY()), g.Point(mid.getX(), mid.getY() - pos_Increment), mid_Top, colour]),
-        unwrap.__call__(draw_Polygon, [window, mid_Top, g.Point(top_Right.getX() - pos_Increment, top_Right.getY()), g.Point(mid.getX(), mid.getY() - pos_Increment), colour]),
-        unwrap.__call__(draw_Polygon, [window, g.Point(top_Right.getX(), top_Right.getY() + pos_Increment), g.Point(mid.getX() + pos_Increment, mid.getY()), mid_Right, colour]),
-        unwrap.__call__(draw_Polygon, [window, g.Point(bottom_Left.getX(), bottom_Left.getY() - pos_Increment), g.Point(mid.getX() - pos_Increment, mid.getY()), mid_Left, colour]),
-        unwrap.__call__(draw_Polygon, [window, g.Point(bottom_Left.getX() + pos_Increment, bottom_Left.getY()), g.Point(mid.getX(), mid.getY() + pos_Increment), mid_Bottom, colour]),
-        unwrap.__call__(draw_Polygon, [window, mid_Right, g.Point(mid.getX() + pos_Increment, mid.getY()), g.Point(bottom_Right.getX(), bottom_Right.getY() - pos_Increment), colour]),
-        unwrap.__call__(draw_Polygon, [window, g.Point(bottom_Right.getX() - pos_Increment, bottom_Right.getY()), g.Point(mid.getX(), mid.getY() + pos_Increment), mid_Bottom, colour])
+        draw_Polygon(window, g.Point(top_Left.getX(), top_Left.getY() + pos_Increment),g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour),
+        draw_Polygon(window, g.Point(top_Left.getX() + pos_Increment, top_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), top_Mid, colour),
+        draw_Polygon(window, top_Mid, g.Point(top_Right.getX() - pos_Increment, top_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), colour),
+        draw_Polygon(window, g.Point(top_Right.getX(), top_Right.getY() + pos_Increment), g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), mid_Right, colour),
+        draw_Polygon(window, g.Point(bottom_Left.getX(), bottom_Left.getY() - pos_Increment), g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour),
+        draw_Polygon(window, g.Point(bottom_Left.getX() + pos_Increment, bottom_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour),
+        draw_Polygon(window, mid_Right, g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), g.Point(bottom_Right.getX(), bottom_Right.getY() - pos_Increment), colour),
+        draw_Polygon(window, g.Point(bottom_Right.getX() - pos_Increment, bottom_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour)
     }
+
+    # poly_List = [
+    #     'section_one': draw_Polygon(window, g.Point(top_Left.getX(), top_Left.getY() + pos_Increment),g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour),
+    #     'section_two': draw_Polygon(window, g.Point(top_Left.getX() + pos_Increment, top_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), top_Mid, colour),
+    #     'section_three': draw_Polygon(window, top_Mid, g.Point(top_Right.getX() - pos_Increment, top_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), colour),
+    #     'section_four': draw_Polygon(window, g.Point(top_Right.getX(), top_Right.getY() + pos_Increment), g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), mid_Right, colour),
+    #     'section_five': draw_Polygon(window, g.Point(bottom_Left.getX(), bottom_Left.getY() - pos_Increment), g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour),
+    #     'section_six': draw_Polygon(window, g.Point(bottom_Left.getX() + pos_Increment, bottom_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour),
+    #     'section_seven': draw_Polygon(window, mid_Right, g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), g.Point(bottom_Right.getX(), bottom_Right.getY() - pos_Increment), colour),
+    #     'section_eight': draw_Polygon(window, g.Point(bottom_Right.getX() - pos_Increment, bottom_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour)
+    # ]
+    #
+    # poly_List = {
+    #     unwrap.__call__(draw_Polygon, [window, g.Point(top_Left.getX(), top_Left.getY() + pos_Increment), g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour]),
+    #     unwrap.__call__(draw_Polygon, [window, g.Point(top_Left.getX() + pos_Increment, top_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), top_Mid, colour]),
+    #     unwrap.__call__(draw_Polygon, [window, top_Mid, g.Point(top_Right.getX() - pos_Increment, top_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() - pos_Increment), colour]),
+    #     unwrap.__call__(draw_Polygon, [window, g.Point(top_Right.getX(), top_Right.getY() + pos_Increment), g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), mid_Right, colour]),
+    #     unwrap.__call__(draw_Polygon, [window, g.Point(bottom_Left.getX(), bottom_Left.getY() - pos_Increment), g.Point(mid_Mid.getX() - pos_Increment, mid_Mid.getY()), mid_Left, colour]),
+    #     unwrap.__call__(draw_Polygon, [window, g.Point(bottom_Left.getX() + pos_Increment, bottom_Left.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour]),
+    #     unwrap.__call__(draw_Polygon, [window, mid_Right, g.Point(mid_Mid.getX() + pos_Increment, mid_Mid.getY()), g.Point(bottom_Right.getX(), bottom_Right.getY() - pos_Increment), colour]),
+    #     unwrap.__call__(draw_Polygon, [window, g.Point(bottom_Right.getX() - pos_Increment, bottom_Right.getY()), g.Point(mid_Mid.getX(), mid_Mid.getY() + pos_Increment), bottom_Mid, colour])
+    # }
 
     for quadrant in range(8):
         quadrant += 1
