0ba19dd751d041aeaa95985bcd5ae19476f31d0f
Will <will.green98@hotmail.com>
2018-01-03 09:48:11 +0000

    String updates

--- Week 8/coin_flip.py
+++ Week 8/coin_flip.py
@@ -1,13 +1,11 @@
 import random
 
-
 def getInputs():
     while True:
-        inputs = input("How many times do you want to flip the coin:")
-        if inputs.isnumeric():
-            return int(inputs)
-        print("Incorrect values please try again")
-
+        flips = input("Number of flips: ")
+        if flips.isnumeric():
+            return int(flips)
+        print("No.")
 
 def simulateFlips(count):
     flips = []
@@ -15,18 +13,15 @@ def simulateFlips(count):
         flips.append(random.randint(0, 1))
     return flips
 
-
 def displayResults(results):
     head_count = results.count(0) / 100
     tail_count = results.count(1) / 100
     print("There were {0} heads and {1} tails".format(head_count, tail_count))
 
-
 def main():
     count = getInputs()
     flips = simulateFlips(count)
     displayResults(flips)
-
 
 if __name__ == '__main__':
     main()
