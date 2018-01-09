9b37ad5aed339b324a9c0fdb4a7f0b5b0d7b25ec
Will <will.green98@hotmail.com>
2018-01-09 17:31:25 +0000

    Returning error

--- Boredom1_Classes.py
+++ Boredom1_Classes.py
@@ -1,19 +1,30 @@
 class person:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, f_Name, s_Name, subject, _isAwesome):
+        self.fName = f_Name
+        self.sName = s_Name
+        self.subject = subject
+        self.isAwesome = bool(_isAwesome)
 
 
-Will = person.name = "Will"
+    def killMyself(person):
+        isAllowed2Die = False
+        if person.isAwesome == "False":
+            isAllowed2Die = True
+            if isAllowed2Die == True:
+                mystr = " is now dead"
+            else:
+                mystr = " is awesome, not allowed to be killed."
 
-def killMyself(name):
-    person.name = name
-    mystr = " is now dead"
+            killed = print("{0}{1}".format(person.fName, mystr))
 
-    killed = print("{0}{1}".format(name, mystr))
-    return killed
+        return killed
+
+Will = person("Will", "Green", ["Comp Sci"], False)
+Bob = person("Bob", "Smith", ["Turfgrass Science"], True)
 
 def main():
-    killMyself(Will)
+    person.killMyself(Will)
+    person.killMyself(Bob)
 
 if __name__ == '__main__':
     main()
