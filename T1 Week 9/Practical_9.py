0def37644090d42854572f624b84b1bca297eb85
Will <will.green98@hotmail.com>
2018-01-20 17:43:48 +0000

    Week 9 upload

---
+++ T1 Week 9/Practical_9.py
@@ -0,0 +1,46 @@
+import graphics as g
+
+def displayDate(day, month, year):
+    months = {
+        1: "Janurary",
+        2: "Feburay",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+
+    print(day, months[month], year)
+
+def wordLength(words = []):
+    while len(words) <= 2:
+        word = input("Enter word: ")
+        words.append(word)
+
+    for i in range(len(words)):
+        print("word: {0} len: {1}".format(words[i], len(words[i])))
+
+# TODO doesn't work
+def drawHexagon():
+    point = []
+
+    window = g.GraphWin("Hexagon", 400, 400)
+
+    for i in range(0, 5, 1):
+        point[i] = window.getMouse()
+
+        return point[i]
+
+    points = point
+    poly = g.Polygon(g.Point(points).getX()).draw(window).setFill("red")
+
+displayDate(14, 2, 2011)
+wordLength()
+drawHexagon()
+
