aea67c3bfb91244afbac8fb6877c43cf2914ebc0
Will <will.green98@hotmail.com>
2018-03-28 23:02:40 +0100

    OG Updates

--- Boredom1_Classes.py
+++ Boredom1_Classes.py
@@ -3,7 +3,7 @@ subjects = []
 isAllowed2Die = True
 
 class Animal:
-    isAllowed2Die = False
+    isAllowed2Die = True
     isPet = bool
 
     def __init__(self, genome, classes, bio_def, c_type):
@@ -12,14 +12,56 @@ class Animal:
         self.bio_def = bio_def
         self.type = c_type
 
-
 class Dog(Animal):
     isPet = True
 
-    def __init__(self, breed):
+    def __init__(self, name, breed):
         super().__init__(self, "Canis", "Carnivore", "Dog")
+        self.name = name
         self.breed = breed
 
+    def bork(d):
+        if d == "quiet":
+            print("Bork Bork Bork... My name is: {0}".format(name))
+        elif d == "loud":
+            print("BORK BORK BORK... MY NAME IS: {0}".format(name))
+        elif d == "sassy":
+            print("Bork Bork Boooork... My name is: {0}".format(name))
+        else:
+            print("Bork")
+
+    def sit(duration):
+        import time
+        
+        print("I am now sitting")
+        
+    def roll_Over():
+        print("My belly is showing")
+        
+    def bundle():
+        print("CUDDLES")
+        
+    def fetch():
+        print("Did you throw something?")
+
+    def zoomies():
+        y = 6
+        for x in range(0, y, 1):
+            print("Z")
+        for x in range(0, y*2, 1):
+            print("O")
+        for x in range(0, y-(y/2), 1):
+            print("M")
+        for x in range(0, y*(y-1/y), 1):
+            print("I")
+        for x in range(0, y-(y*(1/3)), 1):
+            print("E")
+        for x in range(0, y, 1):
+            print("S")
+    
+    def getTheFuckAwayFromMyPizza():
+        print("*DRIBBLES ON FLOOR*")
+        
 class Person:
     def __init__(self, f_Name, s_Name, subject, isAwesome):
         self.fName = f_Name
@@ -31,32 +73,27 @@ class Person:
 
     def killMyself(self):
         if self._isAwesome:
-            ToBkilledOrNotToBKilledThatIsTheQuestion = " is awesome, not allowed to be killed."
+            ToBkilledOrNotToBKilledThatIsTheQuestion = " is awesome, they are not allowed to be killed."
         else:
             ToBkilledOrNotToBKilledThatIsTheQuestion = " is now dead."
 
-        killed = print("{0}{1}".format(self.fName, ToBkilledOrNotToBKilledThatIsTheQuestion))
+        killed = "{0}{1}".format(self.fName, ToBkilledOrNotToBKilledThatIsTheQuestion)
         return killed
 
-
-Will = Person("Will", "Greatest", ["Comp Sci"], True)
+Albert = Person("Albert", "ATacoMan", ["Comp Sci & AI"], True)
 Kewal = Person("Kewal", "Bitch-Boi", ["Maths"], True)
-Bob = Person("Bob", "Smith", ["Turfgrass Science"], False)
+Will = Person("Will", "The Bald Bean", ["Comp Sci"], False)
+Xho = Person("Xho", "Squidward", ["Sociology & Pysch"], True)
 
-Loki = Dog("Samoyed")
-Thor = Dog("Samoyed")
-
-
-'''
-Is there a way to access all classes? E.g
-If class name eqiuals animal isAllowed2Die is automatically False
-'''
+Crumble = Dog("Crumble", "Wire-Haired Sausage")
+Loki = Dog("Loki", "Samoyed")
+Rollie = Dog("Rollie", "Sausage")
+Thor = Dog("Thor", "Samoyed")
 
 def main():
     print(subjects, "\n")
 
-    Person.killMyself(Will)
-    Person.killMyself(Kewal)
+    Person.killMyself
 
 if __name__ == '__main__':
     main()
